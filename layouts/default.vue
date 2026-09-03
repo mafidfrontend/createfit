d9d6939769837ff4d6098bae061c5f7fbbae82ae
mafidfrontend <neo.uz001@gmail.com>
2026-09-03 15:05:59 +0500

    removing menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <div class="cf-content">
       <header class="cf-header">
         <NuxtLink to="/" class="cf-logo">Fabrika</NuxtLink>
-        <NuxtLink to="/orders" aria-label="Открыть заказы" class="cf-menu text-lg font-bold leading-none text-ink">≡<span v-if="hasSelection" class="absolute ml-1 mt-[-3px] inline-flex h-1.5 w-1.5 rounded-full bg-sage" /></NuxtLink>
+        <!-- <NuxtLink to="/orders" aria-label="Открыть заказы" class="cf-menu text-lg font-bold leading-none text-ink">≡<span v-if="hasSelection" class="absolute ml-1 mt-[-3px] inline-flex h-1.5 w-1.5 rounded-full bg-sage" /></NuxtLink> -->
       </header>
       <main class="px-6"><slot /></main>
       <BottomNav />
